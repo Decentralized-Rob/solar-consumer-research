ec19fb4cce02d0f351134ac1e346610117f9dd91
Decentralized Rob <105752716+Decentralized-Rob@users.noreply.github.com>
2026-09-06 16:02:21 -0400

    Link Sunrun ethics guide from research navigation

--- components/home/navigation.tsx
+++ components/home/navigation.tsx
@@ -68,6 +68,7 @@ export function HomeHeader({
               Solar Sales, Financing and What Happens After a Complaint
             </a>
             <a href="/companies/sunrun" onClick={closeMenu}>Sunrun</a>
+            <a href="/companies/sunrun/ethics-compliance" onClick={closeMenu}>Sunrun Ethics &amp; Compliance Guide</a>
             <a href="/cases/titan-solar-power" onClick={closeMenu}>Titan Solar Power</a>
             <a href="/cases/connecticut-attorney-general-sunrun-lawsuit" onClick={closeMenu}>
               Connecticut AG v. Sunrun
