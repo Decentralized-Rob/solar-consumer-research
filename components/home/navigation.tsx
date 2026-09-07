85d0d19e2275921ced024c08615edeb9e269213a
Decentralized Rob <105752716+Decentralized-Rob@users.noreply.github.com>
2026-09-07 10:20:09 -0400

    Fix Research menu: add Sunrun 25-year contracts article

--- components/home/navigation.tsx
+++ components/home/navigation.tsx
@@ -69,6 +69,7 @@ export function HomeHeader({
               Solar Sales, Financing and What Happens After a Complaint
             </a>
             <a href="/companies/sunrun" onClick={closeMenu}>Sunrun</a>
+            <a href="/research/sunrun-25-year-solar-contracts" onClick={closeMenu}>Sunrun’s 25-Year Solar Contracts</a>
             <a href="/companies/sunrun/ethics-compliance" onClick={closeMenu}>Sunrun Ethics &amp; Compliance Guide</a>
             <a href="/cases/titan-solar-power" onClick={closeMenu}>Titan Solar Power</a>
             <a href="/cases/connecticut-attorney-general-sunrun-lawsuit" onClick={closeMenu}>
